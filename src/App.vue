413df7279746b46a2674e75f3dabb7224c5ca21d
josebossle <bossledesenvolvimentoweb@gmail.com>
2023-10-31 11:33:08 -0300

    First project em vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header>
+      <h1>Minha Lista de Tarefas</h1>
+    </header>
+    <task-list :tasks="tasks" @addTask="addTask" @removeTask="removeTask" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskList from "./components/TaskList.vue";
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      tasks: [],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    TaskList,
+  },
+  methods: {
+    addTask(task) {
+      this.tasks.push(task);
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "@/assets/styles.css";
 </style>
+
---
+++ src/components/TaskList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="task-list">
+    <div class="task">
+      <input v-model="newTask" @keyup.enter="addTask" placeholder="Adicionar uma nova tarefa" />
+    </div>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task }}
+        <button @click="removeTask(index)">Remover</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["tasks"],
+  data() {
+    return {
+      newTask: "",
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== "") {
+        this.$emit("addTask", this.newTask);
+        this.newTask = "";
+      }
+    },
+    removeTask(index) {
+      this.$emit("removeTask", index);
+    },
+  },
+};
+</script>
+
+<style>
+  @import "@/assets/styles.css";
+</style>
